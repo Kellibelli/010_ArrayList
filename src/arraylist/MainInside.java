7ed8b5ffe550462cd1b6666ea6780b7395e9b081
compsci <compsci@compsci>
2018-01-30 09:34:12 -0700

    BACKUP

--- src/arraylist/MainInside.java
+++ src/arraylist/MainInside.java
@@ -1,33 +1,52 @@
 package arraylist;
-
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
 public class MainInside {
-
-	public static void main(String[] args) {
-		
-		gradingRequirements();
-		
-		StudentFirstNames a2 = new StudentFirstNames();
-		a2.showSize();
-		a2.showContents();
-		a2.addName();
-		a2.showContents();
-		a2.showSize();
-		a2.removeName();
-		a2.showContents();
-		a2.getName();
-		a2.replaceName();
-		a2.showContents();
-	}
-
-	private static void gradingRequirements() {
-		System.out.println("1. All JOP and jar'd\n2. First is a menu asking for desired option -- looped\n3. Menu: Show Names, Add Name, Remove Name, Replace Name, exit ");
-	}
-
+    
+    public static void main(String[] args) {
+        boolean running = true;
+        String msg = "";
+        StudentFirstNames a2 = new StudentFirstNames();
+       //UIManager.put("OptionPane.background", new ColorUIResource(2, 144, 231));
+        //UIManager.put("Panel.background", new ColorUIResource(2, 144, 231));
+        while (running == true) {
+            Object[] possibilities = { "Add student", "Delete a student", "List students", "Replace a student",
+                    "Exit" };
+            String showFirst = "Add student";
+            String s = (String) JOptionPane.showInputDialog(null, "Please make your selection.", "Start Here",
+                    JOptionPane.PLAIN_MESSAGE, null, possibilities, showFirst);
+            if (s.equalsIgnoreCase("Add student")) {
+                a2.Ask();
+            } else if (s.equalsIgnoreCase("Delete a student")) {
+                a2.removeName();
+            } else if (s.equalsIgnoreCase("List students")) {
+                a2.StudentList();
+            } else if (s.equalsIgnoreCase("Replace a student")) {
+                a2.ReplaceName();
+            } else {
+                System.exit(0);
+            }
+            msg = "Do you want go again?";
+            String goAgain = JOptionPane.showInputDialog(null, msg).toLowerCase();
+            if (goAgain.equals("y") || (goAgain.equals("yes"))) {
+                running = true;
+            } else if (goAgain.equals("n") || (goAgain.equals("no"))) {
+                running = false;
+                msg = "Have a good day!";
+                JOptionPane.showMessageDialog(null, msg);
+            } else {
+                msg = "Please try again";
+                JOptionPane.showMessageDialog(null, msg);
+            }
+        }
+    }
+    /*
+     * private static void GradingRequrements() { System.out.println(
+     * "1. ALL JOP and jar'd\n" +
+     * "2. First is a menu asking for desired option, looped" +
+     * "3. menu: Show Names, Add name, Remove name, Replace name, exit"
+     *
+     * );
+     */
 }
-
-/*
- * 1. add this functionality -- Ask for a name, add that name to ArrayList
- * 1a. test -- after each name, show size and contents
- * 
- * 
- */
--- src/arraylist/StudentFirstNames.java
+++ src/arraylist/StudentFirstNames.java
@@ -1,102 +1,80 @@
 package arraylist;
-
 import java.util.ArrayList;
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
-
 public class StudentFirstNames {
-	
-
-	private ArrayList<String> a2Students = new ArrayList<String>();
-	Scanner kbd = new Scanner(System.in);
-	String n = "";
-	
-
-	public StudentFirstNames() {
-		// constructor
-
-		a2Students.add("Zachary");
-		a2Students.add("Kyle");
-		a2Students.add("Michael");
-		a2Students.add("Mitchell");
-
-	}
-
-	public void showContents() {
-		// method
-		
-		//for (int i = 0; i < a2Students.size(); i++) {
-			JOptionPane.showMessageDialog(null, "Contains: " + a2Students);
-			//System.out.println("Contains: " + a2Students.get(i));
-		//}
-
-	}
-
-	public void addName() {
-		String entry = JOptionPane.showInputDialog("What is your name student?");
-		//System.out.println("What is your name student?");
-		//String entry = (kbd.nextLine());
-		if (a2Students.indexOf(entry) < 0) {
-			a2Students.add(entry);
-		} else {
-			JOptionPane.showConfirmDialog(null, "Name already exists");
-		}
-	
-
-	}
-
-	public void getName() {
-		String n = JOptionPane.showInputDialog("What name would you like to search for?");
-		if (a2Students.indexOf(n) < 0) {
-			JOptionPane.showMessageDialog(null, "Sorry, name not found.");
-		} else {
-			JOptionPane.showConfirmDialog(null, "Name Found!");
-		}
-	}
-
-	public void removeName() {
-		String name = JOptionPane.showInputDialog("Which name would you like to remove?");
-		//System.out.println("Which name would you like to remove?");
-		//String name = (kbd.nextLine());
-		if (a2Students.indexOf(name) < 0) {
-			System.out.println("Name doesn't exist");
-
-		} else {
-			a2Students.remove(name);
-
-		}
-		
-
-	}
-
-	public void replaceName() {
-		System.out.println("Enter a name to replace");
-		String  name = (kbd.nextLine());
-		int index = a2Students.indexOf(name);
-		if (index== -1 ) {
-			System.out.println("You did't put in a name from the list");
-			return;
-		}
-		System.out.println("Please enter a name");
-		name = kbd.nextLine();
-		a2Students.set(index, name);
-		
-
-	}
-
-	public void showSize() {
-		JOptionPane.showMessageDialog( null, "Number of student names is: " + a2Students.size());
-		//System.out.println("Number of student names is: " + a2Students.size());
-
-	}
-
-	public ArrayList<String> getA2Students() {
-		return a2Students;
-	}
-
-	public void setA2Students(ArrayList<String> a2Students) {
-		this.a2Students = a2Students;
-	}
-
+    private ArrayList<String> a2Students = new ArrayList<String>();
+    public StudentFirstNames() {
+        a2Students.add("Kelli");
+        a2Students.add("Bobby");
+        a2Students.add("Sammy");
+        a2Students.add("Thomas");
+    }
+    public void Ask() {
+        
+        String entry = JOptionPane.showInputDialog("Please enter a name.");
+    
+        if (a2Students.indexOf(entry) < 0) {
+            a2Students.add(entry);
+        } else {
+            JOptionPane.showMessageDialog(null, "name already exists");
+        }
+    }
+    public void StudentSize() {
+        System.out.println("Number of student names is: " + a2Students.size());
+    }
+    public void StudentList() {
+        
+        int i = 0;
+        
+        StringBuilder output = new StringBuilder("All student names: \n");
+        for ( i = 0; i < a2Students.size(); i++) {
+            output.append(a2Students.get(i) + "\n");
+        }
+        output.append("");
+        JOptionPane.showMessageDialog(null, output);
+        
+        
+        
+        
+        //System.out.println("Student names: " + a2Students);
+        //for (int i = 0; i < a2Students.size(); i++)
+            //System.out.println(a2Students.get(i));
+    }
+    public void StudentsCheck() {
+        String n = JOptionPane.showInputDialog("what name are you searching for");
+        if (a2Students.indexOf(n) < 0) {
+            JOptionPane.showMessageDialog(null, "name not found");
+        } else {
+            JOptionPane.showMessageDialog(null, "name found");
+        }
+    }
+    public void removeName() {
+        
+        String name = JOptionPane.showInputDialog("what name do you want to remove?");
+    
+        if (a2Students.indexOf(name) > 0)
+            a2Students.remove(name);
+        else
+            JOptionPane.showMessageDialog(null, "name doesn't exist");
+    }
+    public void ReplaceName() {
+        
+        String name = JOptionPane.showInputDialog("what name do you want to replace?");
+        
+        int index = a2Students.indexOf(name);
+        if (index == -1) {
+            JOptionPane.showMessageDialog(null, "name not on list");
+            //System.out.println("name not on list");
+            return;
+        }
+         name = JOptionPane.showInputDialog("What do you want to replce it with?");
+        
+        a2Students.set(index, name);
+    }
+    public ArrayList<String> getA2Students() {
+        return a2Students;
+    }
+    public void setA2Students(ArrayList<String> a2Students) {
+        this.a2Students = a2Students;
+    }
 }
